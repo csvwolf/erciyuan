905d02eb2e564eab3569b570494077628835afbf
SkyAo <csvwolf@qq.com>
2017-03-23 18:07:05 +0800

    add weather

--- src/index.js
+++ src/index.js
@@ -28,6 +28,24 @@ const getTime = function() {
   document.querySelector('.time').innerHTML = `${leftPad(date.getHours())} ${showSpilter ? ':' : '&nbsp;'} ${leftPad(date.getMinutes())}`;
 };
 
+const getLocation = function() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(pos => resolve(pos));
+  });
+}
+
+const getWeather = function() {
+  getLocation()
+  .then(pos => fetch(`https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22(%20${pos.coords.latitude}%2C${pos.coords.longitude}%20)%22)%20and%20u%3D%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys`))
+  .then(response => response.json())
+  .then(data => {
+    // document.querySelector('.temperature')
+  });
+};
+
+
+
 getBackground();
 getHitokoto();
 setInterval(getTime, 500);
+getWeather();
